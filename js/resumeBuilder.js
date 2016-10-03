69b0a7299092c86bbcc5c140762ac257a35e47e8
Damien1989 <damiengraille@gmail.com>
2016-10-03 22:25:12 +0100

    en cours

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,15 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var awesomeThoughts = "I am Damien and I am awesome";
+ var funThoughts = awesomeThoughts.replace ("awesome","fun");
+ console.log(awesomeThoughts);
+ console.log(funThoughts);
+ $("#main").append(funThoughts);
+ var formattedName = HTMLheaderName.replace ("%data%", "Damien Graille");
+ var formattedRole = HTMLheaderRole.replace ("%data%", "Web Developper");
+ $("#header").prepend(formattedName);
+ $("#header").append(formattedRole);
+
+
+ 
